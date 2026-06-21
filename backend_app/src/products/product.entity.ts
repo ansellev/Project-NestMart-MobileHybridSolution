875606f13dac1eef3bb5e9133d30c893de130683
Shendrian-k <shendrian.iyan123@gmail.com>
2026-06-21 18:53:47 +0700

    Update product.entity.ts

--- backend_app/src/products/product.entity.ts
+++ backend_app/src/products/product.entity.ts
@@ -1,1 +1,26 @@
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column()
+  image: string;
+
+  @Column()
+  category: string;
+
+  @Column()
+  storeName: string;
+
+  @Column()
+  stock: number;
+}
