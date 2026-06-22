d2e799a7c2669a1759ae5260f89b92b89c3fc03b
Shendrian-k <shendrian.iyan123@gmail.com>
2026-06-22 14:10:19 +0700

    Update favorites.module.ts

--- backend_app/src/favorites/favorites.module.ts
+++ backend_app/src/favorites/favorites.module.ts
@@ -1,1 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Favorite } from './favorite.entity';
+import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Favorite,
+    ]),
+  ],
+  controllers: [
+    FavoritesController,
+  ],
+  providers: [
+    FavoritesService,
+  ],
+})
+export class FavoritesModule {}
