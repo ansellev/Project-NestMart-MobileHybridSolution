277c7d6483d85b6469249c70489acd27eb9fdb50
Shendrian-k <shendrian.iyan123@gmail.com>
2026-06-22 11:11:13 +0700

    Update order.entity.ts

--- backend_app/src/orders/order.entity.ts
+++ backend_app/src/orders/order.entity.ts
@@ -1,1 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
 
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column('decimal')
+  total: number;
+
+  @Column({
+    default: 'Paid',
+  })
+  status: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
