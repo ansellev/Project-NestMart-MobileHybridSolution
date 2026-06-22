0cbe13aab14f0bf2f3133b80e97bc87bf8cd7ce1
Shendrian-k <shendrian.iyan123@gmail.com>
2026-06-22 14:09:17 +0700

    Update favorites.controller.ts

--- backend_app/src/favorites/favorites.controller.ts
+++ backend_app/src/favorites/favorites.controller.ts
@@ -1,1 +1,44 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 
+import { FavoritesService } from './favorites.service';
+
+@Controller('favorites')
+export class FavoritesController {
+  constructor(
+    private readonly favoritesService: FavoritesService,
+  ) {}
+
+  @Post()
+  addFavorite(
+    @Body() body: any,
+  ) {
+    return this.favoritesService.addFavorite(
+      body,
+    );
+  }
+
+  @Get(':userId')
+  getFavorites(
+    @Param('userId') userId: string,
+  ) {
+    return this.favoritesService.getFavorites(
+      Number(userId),
+    );
+  }
+
+  @Delete(':id')
+  deleteFavorite(
+    @Param('id') id: string,
+  ) {
+    return this.favoritesService.deleteFavorite(
+      Number(id),
+    );
+  }
+}
