8764a91d2af29c93456ff8a5ecce930b8ac8f9f4
Shendrian-k <shendrian.iyan123@gmail.com>
2026-06-22 11:19:23 +0700

    Update order-item.entity.ts

--- backend_app/src/orders/order-item.entity.ts
+++ backend_app/src/orders/order-item.entity.ts
@@ -1,1 +1,23 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
 
+@Entity('order_items')
+export class OrderItem {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  orderId: number;
+
+  @Column()
+  productId: number;
+
+  @Column()
+  quantity: number;
+
+  @Column('decimal')
+  price: number;
+}
