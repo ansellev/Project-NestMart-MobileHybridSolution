387096197e97a4ef31617bdd412f1887fa5faac4
Shendrian-k <shendrian.iyan123@gmail.com>
2026-06-25 21:13:51 +0700

    Update app.module.ts

--- backend_app/src/app.module.ts
+++ backend_app/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './products/product.module';
 import { CartModule } from './cart/cart.module';
 import { OrdersModule } from './orders/orders.module';
 import { FavoritesModule } from './favorites/favorites.module';
